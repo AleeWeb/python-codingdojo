a2d368671093a5103fa62d2e372dbdf49e5e4d7f
AleeWeb <aalexanderpr@gmail.com>
2017-08-02 23:04:17 -0400

    The starter code. To be continued...

---
+++ find-char.py
@@ -0,0 +1,19 @@
+'''
+Write a program that takes a list of strings and a string containing a single character, and 
+prints a new list of all the strings containing that character.
+'''
+
+#TO BE CONTINUED - BED TIME. GETTING UP EARLY FOR WORK
+
+def findChar():
+
+critterList = ['pandas', 'puppies', 'chicklets', 'hamsters']
+
+singleChar = ['a']
+
+
+
+findChar()
+
+
+
