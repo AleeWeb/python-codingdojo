5ceacfdfeb28e24b05df32e9b879a511e79c9a1e
AleeWeb <aalexanderpr@gmail.com>
2017-08-06 00:04:53 -0400

    Fix typo comment replace 9 with 8

--- checkerboard.py
+++ checkerboard.py
@@ -2,7 +2,7 @@
 #assignment to print a cool checkerboard made of asterisks
 
 def checkerBoard():
-    for i in range of 8:  #Because there are 9 rows of stars in the assignment requirement
+    for i in range of 8:  #Because there are 8 rows of stars in the assignment requirement
 
 
 
