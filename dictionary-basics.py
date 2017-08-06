f2ae8a3316636f251dd880f3031edc15dcf2f468
AleeWeb <aalexanderpr@gmail.com>
2017-08-05 23:59:47 -0400

    Code

---
+++ dictionary-basics.py
@@ -0,0 +1,15 @@
+def printDict(person):
+    for key in dictionary:
+        print 'My ', key, ' is ', dictionary[key]
+
+
+anna = {
+    'name': 'Anna',
+    'Age': 101,
+    'Country of Birth': 'United States',
+    'favorite language': 'Python',
+    }
+
+printDict(anna)
+
+			
