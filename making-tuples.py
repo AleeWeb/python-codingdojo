a173085d33858a1a71faa6c741accd09d253afe6
AleeWeb <aalexanderpr@gmail.com>
2017-08-06 09:47:14 -0400

    Starting function code

---
+++ making-tuples.py
@@ -0,0 +1,10 @@
+
+def dictConvert():
+    animalsDict = {
+        "Giant Panda": "Ailuropoda melanoleuca",
+        "Dog": "Canis lupus familiaris",
+        "Butterfly": "Rhopalocera"
+    }
+
+
+dictConvert()
