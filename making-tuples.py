f82dac281e01f6285cb37479727df8687bf4440e
AleeWeb <aalexanderpr@gmail.com>
2017-08-06 10:08:42 -0400

    Assignment completed

--- making-tuples.py
+++ making-tuples.py
@@ -1,10 +1,23 @@
 
 def dictConvert():
+
     animalsDict = {
         "Giant Panda": "Ailuropoda melanoleuca",
         "Dog": "Canis lupus familiaris",
         "Butterfly": "Rhopalocera"
     }
+    #print animalsDict
 
+    newDict = {}
+    for key in animalsDict.items():
+        newDict[key] = tuple
+
+    animalsDict = newDict
+
+    print newDict
+
+    
+
+    
 
 dictConvert()
