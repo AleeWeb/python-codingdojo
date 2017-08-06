331aae6f51816cb9ab450a9c2db3eca218044264
AleeWeb <aalexanderpr@gmail.com>
2017-08-06 16:34:27 -0400

    Starter code

---
+++ making-dict.py
@@ -0,0 +1,15 @@
+
+favCritters = ["Pandas", "Puppies", "Dalmatians", "Butterflies"]
+names = ["Alicia", "Ashley", "Joe", "Billy", "Becky"]
+
+
+def makeDict(arr1, arr2):
+    newDict = {}
+    for key in favCritters.items():
+        newDict[key] = favCritters
+
+    favCrittes = newDict
+
+    return newDict
+
+makeDict()
