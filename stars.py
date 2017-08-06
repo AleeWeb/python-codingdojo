9b49c569dba6ee470311cae2c73b7eed490f02cd
AleeWeb <aalexanderpr@gmail.com>
2017-08-06 14:19:16 -0400

    Code

--- stars.py
+++ stars.py
@@ -1,8 +1,17 @@
-
-
 def drawStars():
 
-x = [3, 26, 100, 3, 5, 7, 25]
+	numList = [3, 26, 100, 3, 5, 7, 25]
 
+	numList = input("List of integers and strings: ")
+	for i in range(len(numList)):
+		if(isinstance(numList[i],int)):
+			print '*'* numList[i]
+		else:
+			print numList[i][0]*len(numList[i])
 
-drawStars()
+def drawStars(num):
+
+	listX = [4, "Tom", 1, "Michael", 5, 7, "Jimmy Smith"]
+	for item in num:
+
+	
