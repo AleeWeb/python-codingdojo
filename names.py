69a35bff9bb73f26832b03c40e5edad4026cb053
AleeWeb <aalexanderpr@gmail.com>
2017-08-06 14:59:25 -0400

    Assignment code

--- names.py
+++ names.py
@@ -1,6 +1,43 @@
+# Part 1
 students = [
      {'first_name':  'Michael', 'last_name' : 'Jordan'},
      {'first_name' : 'John', 'last_name' : 'Rosales'},
      {'first_name' : 'Mark', 'last_name' : 'Guillen'},
      {'first_name' : 'KB', 'last_name' : 'Tonel'}
 ]
+
+def displayStudents(arr):
+    for student in students:
+        print student['first_name'], student['last_name']
+
+displayStudents(students)
+
+# Part 2
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+
+
+def showUsers(users):
+    for role in users:
+        counter = 0
+        print role
+        for person in users[role]:
+            counter += 1
+            first_name = person['first_name'].upper()
+            last_name = person['last_name'].upper()
+            length = len(first_name) + len(last_name)
+            print "{} - {} {} - {}".format(counter, first_name, last_name, length)
+
+
+showUsers(users)
