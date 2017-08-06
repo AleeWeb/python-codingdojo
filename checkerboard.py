fd75081102430d86435ec19bf20e9276fc3e1143
AleeWeb <aalexanderpr@gmail.com>
2017-08-06 00:03:57 -0400

    Starting checker board function code

---
+++ checkerboard.py
@@ -0,0 +1,12 @@
+
+#assignment to print a cool checkerboard made of asterisks
+
+def checkerBoard():
+    for i in range of 8:  #Because there are 9 rows of stars in the assignment requirement
+
+
+
+
+
+
+checkerBoard()
