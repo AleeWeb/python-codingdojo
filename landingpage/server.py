7f68e944fea3888df0dff7c8d2ca6290e382bf26
AleeWeb <aalexanderpr@gmail.com>
2017-08-07 22:51:33 -0400

    Code Fixes!

--- landingpage/server.py
+++ landingpage/server.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def greeting():
     return render_template('index.html', greeting="Welcome")
-app.run(debug=True)
+ 
 
 @app.route('/ninjas')
 def ninjas():
-    return render_template('ninjas.html', info="The ninja was a covert agent in feudal Japanese times who specialized in unorthodox warfare including espionage, sabotage, infiltration, and assassination. Compared to the samurai who were very upfront and honorable with their tactics, the ninja proved a stark contrast.")
+    return render_template('ninjas.html', info="The ninja was a covert agent in feudal Japanese times who specialized in unorthodox warfare including espionage, sabotage, infiltration, and assassination.")
 app.run(debug=True)
