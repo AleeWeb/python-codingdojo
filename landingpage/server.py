9e00bcc0a3347a52499d017bb20bc84fc14c8c50
AleeWeb <aalexanderpr@gmail.com>
2017-08-07 23:03:31 -0400

    Completed code for all 3 parts

--- landingpage/server.py
+++ landingpage/server.py
@@ -9,4 +9,9 @@ def greeting():
 @app.route('/ninjas')
 def ninjas():
     return render_template('ninjas.html', info="The ninja was a covert agent in feudal Japanese times who specialized in unorthodox warfare including espionage, sabotage, infiltration, and assassination.")
+
+@app.route('/dojos/new')
+def form():
+    return render_template('dojos.html', form="Field")
+
 app.run(debug=True)
