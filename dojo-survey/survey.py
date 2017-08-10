53b710bed2d64c49a3ae3d2ad5e438ca385da793
AleeWeb <aalexanderpr@gmail.com>
2017-08-09 21:16:08 -0400

    Completed Assignment
    Completed Dojo Survey assignment

--- dojo-survey/survey.py
+++ dojo-survey/survey.py
@@ -1,6 +1,23 @@
-from Flask import Flask, render_template
-app = Flask (__name__)
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'appKey'
 
 @app.route('/')
-def something():
-    return render_template('survey.html', blank="welcome")
+def surveyOptions():
+    optionList = ['D.C.', 'Chicago', 'San Jose', 'New York', 'Seattle']
+    langList = ['JavaScript', 'Python', 'Java', 'PHP']
+    return render_template('survey.html', optionList=optionList, langList=langList)
+
+@app.route('/create', methods=['POST'])
+def createUser():
+    session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    session['comment'] = request.form['comment']
+    return redirect('/process')
+
+@app.route('/process')
+def showUser():
+    return render_template('results.html')
+
+app.run(debug=True)
